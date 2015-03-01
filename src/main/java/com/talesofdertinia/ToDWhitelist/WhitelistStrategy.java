acc1a7afbf860cc81c182b3db262879652e11155
Johan Fogelström <zapmew@gmail.com>
2015-03-02 00:54:03 +0100

    Updated WhitelistStrategy with the new method.

--- src/main/java/com/talesofdertinia/ToDWhitelist/WhitelistStrategy.java
+++ src/main/java/com/talesofdertinia/ToDWhitelist/WhitelistStrategy.java
@@ -2,17 +2,32 @@ package com.talesofdertinia.ToDWhitelist;
 
 import org.bukkit.World;
 
+import com.talesofdertinia.ToDWhitelist.User.Status;
+
 public class WhitelistStrategy implements Strategy {
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public boolean isAllowed(User user) {
-		return user.inWhitelist();
+		if (user.getStatus() == Status.Whitelisted) {
+			return true;
+		} else if (user.getPlayer().isOp()) {
+			ToDWhitelist.getStaticLogger().warning("Non-whitelisted Op joined. Remember to register all ops in whitelist or deop if no longer welcome.");
+			return true;
+		}
+		return false;
 	}
-	
+
 	public boolean isAllowed(User user, World w) {
 		return isAllowed(user);
+	}
+
+	@Override
+	public Status getInviteStatus() {
+		//FIXME Perhaps move into separate class or passed as constructor parameter.
+		//Default behaviour Assume good.
+		return Status.Whitelisted;
 	}
 
 }
