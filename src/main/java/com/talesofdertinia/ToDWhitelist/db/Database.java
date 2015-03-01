d98dee628cd91779823bc61c9c2b209245d35c09
Johan Fogelström <zapmew@gmail.com>
2015-03-02 00:48:23 +0100

    Moved and reworked Database a bit

---
+++ src/main/java/com/talesofdertinia/ToDWhitelist/db/Database.java
@@ -0,0 +1,22 @@
+package com.talesofdertinia.ToDWhitelist.db;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.bukkit.OfflinePlayer;
+
+import com.talesofdertinia.ToDWhitelist.Invite;
+import com.talesofdertinia.ToDWhitelist.User;
+import com.talesofdertinia.ToDWhitelist.User.Status;
+
+public interface Database {
+	public User getUser(UUID uuid);
+	public User getUser(OfflinePlayer player);
+	public boolean insert(User user);
+	public boolean update(User u);
+	
+	public Invite getInvite(User invited);
+	public List<Invite> getInvites(User inviter);
+	public boolean insert(Invite invite);
+	public boolean update(Invite invite);
+}
